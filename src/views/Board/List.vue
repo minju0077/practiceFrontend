6024b99d788138174b014690f739a8e931a9fc8d
mj <qjsalswn@gmail.com>
2026-02-24 14:46:38 +0900

    [Feat] 게시글 목록 조회 페이지 및 기능 추가

---
+++ src/views/Board/List.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { reactive } from 'vue'
+import api from '@/api/board'
+
+const boardList = reactive([]);
+
+const getList = async ()=>{
+    const res = await api.list();
+    boardList.push(...res.data);
+}
+
+getList();
+</script>
+
+<template>
+<table>
+    <thead>
+        <tr>
+            <th>idx></th>
+            <th>제목</th>
+            <th>내용</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="board in boardList">
+            <td>{{ board.idx }}</td>
+            <td>{{ board.title }}</td>
+            <td>{{ board.content }}</td>
+        </tr>
+    </tbody>
+</table>
+</template>
+
+<style scoped>
+
+</style>
